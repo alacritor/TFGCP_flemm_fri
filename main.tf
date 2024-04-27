114c815270cbde5f9669dd6fcfd328b280508acc
alacritor <alacrity.dh@gmail.com>
2024-04-26 20:23:47 -0700

    diggler

--- main.tf
+++ main.tf
@@ -38,3 +38,38 @@ resource "google_storage_bucket" "tragic-mulot" {
     }
   }
 }
+
+
+resource "google_storage_bucket" "diggler-bucket2" {
+  name          = "darth_diggler"
+  location      = "us-west1"
+  force_destroy = true
+
+}
+
+
+resource "google_compute_network" "diggler-auto-vpc-tf" {
+  name = "diggler-auto-vpc-tf"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "diggler-sub-sg" {
+  name ="diggler-sub-sg"
+  network = google_compute_network.auto-vpc-tf.id
+  ip_cidr_range = "10.161.1.0/24"
+  region = "southamerica-east1"
+}
+
+
+#resource "google_compute_network" "custom-vpc-tf" {
+  #name = "custom-vpc-tf"
+ #auto_create_subnetworks = false
+#}
+
+output "auto" {
+  value = google_compute_network.auto-vpc-tf.id
+}
+
+#output "custom" {
+#  value = google_compute_network.custom-vpc-tf.id
+#}
